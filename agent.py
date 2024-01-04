c7297012e51084a279f3f152a33c645e3a190608
jjosecodes <josejob123121@gmail.com>
2024-01-03 19:28:00 -0600

    ss added

--- agent.py
+++ agent.py
@@ -14,9 +14,9 @@ class Agent:
 
     def __init__(self):
         self.n_games = 0
-        self.epsilon = 0  # randomness
-        self.gamma = 0.9  # discount rate
-        self.memory = deque(maxlen=MAX_MEMORY) 
+        self.epsilon = 0 # randomness
+        self.gamma = 0.9 # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY) # popleft()
         self.model = Linear_QNet(11, 256, 3)
         self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
 
