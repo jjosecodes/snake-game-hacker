a71353d12df7c9949425809f23fe219e56b733a9
jjosecodes <josejob123121@gmail.com>
2024-01-03 22:11:00 -0600

    v1

--- agent.py
+++ agent.py
@@ -11,15 +11,13 @@ BATCH_SIZE = 1000
 LR = 0.001
 
 class Agent:
-
     def __init__(self):
         self.n_games = 0
-        self.epsilon = 0 # randomness
-        self.gamma = 0.9 # discount rate
-        self.memory = deque(maxlen=MAX_MEMORY) # popleft()
-        self.model = Linear_QNet(11, 256, 3)
+        self.epsilon = max(self.epsilon - 0.001, 0.1)  # randomness
+        self.gamma = 0.9  # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY)
+        self.model = Linear_QNet(15, 256, 3)  # Make sure the input size matches the state size
         self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
-
 
     def get_state(self, game):
         head = game.snake[0]
@@ -62,32 +60,37 @@ class Agent:
             game.food.x < game.head.x,  # food left
             game.food.x > game.head.x,  # food right
             game.food.y < game.head.y,  # food up
-            game.food.y > game.head.y  # food down
+            game.food.y > game.head.y,  # food down
             ]
+
+        # Additional state information about body
+        body_up = dir_u and game.is_collision(point_u, True)
+        body_down = dir_d and game.is_collision(point_d, True)
+        body_right = dir_r and game.is_collision(point_r, True)
+        body_left = dir_l and game.is_collision(point_l, True)
+
+        state.extend([body_up, body_down, body_right, body_left])
 
         return np.array(state, dtype=int)
 
     def remember(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done)) # popleft if MAX_MEMORY is reached
+        self.memory.append((state, action, reward, next_state, done))
 
     def train_long_memory(self):
         if len(self.memory) > BATCH_SIZE:
-            mini_sample = random.sample(self.memory, BATCH_SIZE) # list of tuples
+            mini_sample = random.sample(self.memory, BATCH_SIZE)
         else:
             mini_sample = self.memory
 
         states, actions, rewards, next_states, dones = zip(*mini_sample)
         self.trainer.train_step(states, actions, rewards, next_states, dones)
-        #for state, action, reward, nexrt_state, done in mini_sample:
-        #    self.trainer.train_step(state, action, reward, next_state, done)
 
     def train_short_memory(self, state, action, reward, next_state, done):
         self.trainer.train_step(state, action, reward, next_state, done)
 
     def get_action(self, state):
-        # random moves: tradeoff exploration / exploitation
         self.epsilon = 80 - self.n_games
-        final_move = [0,0,0]
+        final_move = [0, 0, 0]
         if random.randint(0, 200) < self.epsilon:
             move = random.randint(0, 2)
             final_move[move] = 1
@@ -98,7 +101,6 @@ class Agent:
             final_move[move] = 1
 
         return final_move
-
 
 def train():
     plot_scores = []
@@ -132,7 +134,11 @@ def train():
 
             if score > record:
                 record = score
-                agent.model.save()
+                agent.model.save(file_name=f'model_{agent.n_games}.pth')
+
+            if agent.n_games % 100 == 0:
+                print(f"Saving model at game {agent.n_games}")
+                agent.model.save(file_name=f'model_{agent.n_games}.pth')
 
             print('Game', agent.n_games, 'Score', score, 'Record:', record)
 
@@ -141,7 +147,6 @@ def train():
             mean_score = total_score / agent.n_games
             plot_mean_scores.append(mean_score)
             plot(plot_scores, plot_mean_scores)
-
 
 if __name__ == '__main__':
     train()
